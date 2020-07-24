2ebe293baea2deb08a826edaca05a44dce10ef4c
shicheng.yu <shicheng.yu@fawaimanongdeMacBook-Pro.local>
2020-07-24 21:09:29 +0800

    2020年07月24日 21:29的快速提交

---
+++ index.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+require("./utils/initial");
+const selectTemplate = require("@/utils/selectTemplate");
+const inputPackageName = require("@/utils/inputPackageName");
+const downloadTemplate = require("@/utils/downloadTemplate");
+const changeJsonFile = require("@/utils/changeJsonFile");
+
+(async () => {
+  try {
+    const { remote, devDependencies } = await selectTemplate();
+    const packageName = await inputPackageName();
+    await downloadTemplate({ folderName: packageName, remote });
+    await changeJsonFile({ folderName: packageName, projectName: packageName, devDependencies });
+  } catch (error) {
+    throw error;
+  }
+})();
